b61c00b81299ac5b00ef0901ccce8b55763a1fa5
Rithvik <lskrithvik@gmail.com>
2022-12-06 10:07:03 +0000

    Create 4 files

---
+++ hello.js
@@ -0,0 +1,29 @@
+console.log("hellojs")
+console.log(999)
+console.log(undefined)
+console.log(typeof null)
+console.log(typeof true)
+console.log(typeof undefined)
+var a ='2345'
+const b = 64
+let c = 234
+let d
+d = "hello"
+console.log(a,b,c ,d)
+
+if(b>c){
+    console.log("b is greater than c")
+}
+else{
+    console.log("b is less than c");
+}
+for(let i=0 ; i<5 ; i++){
+    console.log("hi")
+}
+
+
+function square(a)
+{
+    return a*a    
+}
+console.log(square(4))
